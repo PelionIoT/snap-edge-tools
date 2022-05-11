c8e7a3a7455e8f725cfa2941f27db222ac76b1f0
Marcus Chang <marcus.chang@pelion.com>
2022-05-11 14:25:57 -0700

    Print openssl errors that are not TLS related

--- scripts/test-bootstrap.sh
+++ scripts/test-bootstrap.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-# exit on error
-set -e
-
 # use openssl to connect to bootstrap server using device credentials
 # store output in $SNAP_USER_COMMON/test-bootstrap.txt
 echo "Write openssl output to $SNAP_USER_COMMON/test-bootstrap.txt"
 echo | openssl s_client -CAfile $SNAP/credentials/bootstrap.pem -key $SNAP/credentials/device01_key.pem -cert $SNAP/credentials/device01_cert.pem -connect tcp-bootstrap.us-east-1.mbedcloud.com:5684 2>$SNAP_USER_COMMON/test-bootstrap.txt > $SNAP_USER_COMMON/test-bootstrap.txt
+
+# get openssl return code
 RESULT=`grep 'Verify return code' $SNAP_USER_COMMON/test-bootstrap.txt`
 
-# get openssl return code and print result
+if [ -z "$RESULT" ]; then
+    echo "openssl failed with:"
+    cat $SNAP_USER_COMMON/test-bootstrap.txt
+    exit
+fi
+
+# print result
 CODE=`echo $RESULT | awk -F' ' '{print $4}'`
 
 if [ $CODE = 0 ]; then
--- scripts/test-lwm2m.sh
+++ scripts/test-lwm2m.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-# exit on error
-set -e
-
 # use openssl to connect to lwm2m server using device credentials
 # store output in $SNAP_USER_COMMON/test-lwm2m.txt
 echo "Write openssl output to $SNAP_USER_COMMON/test-lwm2m.txt"
 echo | openssl s_client -CAfile $SNAP/credentials/lwm2m.pem -key $SNAP/credentials/device01_key.pem -cert $SNAP/credentials/device01_cert.pem -connect lwm2m.us-east-1.mbedcloud.com:5684 2>$SNAP_USER_COMMON/test-lwm2m.txt > $SNAP_USER_COMMON/test-lwm2m.txt
+
+# get openssl return code
 RESULT=`grep 'Verify return code' $SNAP_USER_COMMON/test-lwm2m.txt`
 
-# get openssl return code and print result
+if [ -z "$RESULT" ]; then
+    echo "openssl failed with:"
+    cat $SNAP_USER_COMMON/test-lwm2m.txt
+    exit
+fi
+
+# print result
 CODE=`echo $RESULT | awk -F' ' '{print $4}'`
 
 if [ $CODE = 0 ]; then
